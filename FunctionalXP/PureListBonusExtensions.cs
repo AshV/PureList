72177784a19ea73a4addc6442f98fc75ea926781
AshV <ashish6318@gmail.com>
2017-11-27 01:50:57 +0530

    Filter implemented

--- FunctionalXP/PureListBonusExtensions.cs
+++ FunctionalXP/PureListBonusExtensions.cs
@@ -1,28 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System.Diagnostics.Contracts;
 
 namespace FunctionalXP
 {
     public delegate bool FilterPredicate<in T>(T obj);
 
+    public delegate T MapFunction<in T1, out T>();
+    public delegate T MapFunction<in T1, in T2, out T>();
+    public delegate T MapFunction<in T1, in T2, in T3, out T>();
+    public delegate T MapFunction<in T1, in T2, in T3, in T4, out T>();
+    public delegate T MapFunction<in T1, in T2, in T3, in T4, in T5, out T>();
+
     public static class PureListBonusExtensions
     {
-        public static PureList<T> Filter<T>(this PureList<T> pureList, FilterPredicate<T> predicate)
-        {
-            return FilterApply(
-                      pureList,
-                      predicate,
-                      new PureList<T>(),
-                      pureList.Length - 1);
-        }
+        [Pure]
+        public static PureList<T> Filter<T>(this PureList<T> pureList, FilterPredicate<T> predicate) =>
+                    FilterApply(
+                        pureList,
+                        predicate,
+                        new PureList<T>(),
+                        pureList.Length - 1);
 
-        private static PureList<T> FilterApply<T>(PureList<T> list, FilterPredicate<T> predicate, PureList<T> output, int n)
-        {
-            if (n < 0)
-                return output;
+        [Pure]
+        private static PureList<T> FilterApply<T>(PureList<T> list, FilterPredicate<T> predicate, PureList<T> output, int n) =>
+            (n < 0) ? output : FilterApply(
+                list,
+                predicate,
+                predicate(list[n]) ? output.Cons(list[n]) : output,
+                n - 1);
 
-            if (predicate.Invoke(list[n]))
-                output.Cons(list[n]);
-            return FilterApply(list, predicate, output, n - 1);
-        }
     }
 }
